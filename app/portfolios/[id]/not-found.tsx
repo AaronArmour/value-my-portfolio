daaaf2cccb0dea4be59d39f8edc747b6d77f482c
Aaron Armour <aaronarmour83@gmail.com>
2025-11-11 16:19:17 +1300

    Add main pages with example navigation between them

--- app/portfolios/[id]/not-found.tsx
+++ app/portfolios/[id]/not-found.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function Page() {
+export default function NotFound() {
   return (
     <div className="w-[60vw] mx-auto text-left mt-[5vh]">
-      <h1 className="text-4xl font-bold">Home page</h1>
-      <p>Welcome to Value My Portfolio!</p>
+      <h2 className="text-2xl font-bold">404 Not Found</h2>
+      <p>Could not find the requested portfolio</p>
       <Link href="/portfolios" className="text-blue-500 underline hover:text-blue-700">
-        Go to Portfolios
+        Go back to portfolios
       </Link>
     </div>
   );
---
+++ app/portfolios/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="w-[60vw] mx-auto text-left mt-[5vh]">
+      <h1 className="text-4xl font-bold">Portfolios page</h1>
+
+      <div className="flex flex-col space-2">
+        <Link href="/portfolios/1" className="text-blue-500 underline hover:text-blue-700">
+          Portfolio 1
+        </Link>
+
+        <Link href="/portfolios/2" className="text-blue-500 underline hover:text-blue-700">
+          Portfolio 2
+        </Link>
+
+        <Link href="/portfolios/3" className="text-blue-500 underline hover:text-blue-700">
+          Portfolio 3
+        </Link>
+      </div>
+    </div>
+  );
+}
