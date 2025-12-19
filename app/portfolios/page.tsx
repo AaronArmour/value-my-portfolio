ee6c25e10fce8d62ce4423f3b387019775376853
AaronArmour <63684895+AaronArmour@users.noreply.github.com>
2025-12-19 13:56:47 +1300

    11 display list of portfolios and their current valuations (#12)
    * Shift getInitialPriceMap from app/portfolios/[id]/page.tsx to lib/actions.ts
    
    * Add calculation of portfolio values into portfolios page

--- app/portfolios/page.tsx
+++ app/portfolios/page.tsx
@@ -1,9 +1,18 @@
 import { quattrocento } from "@/ui/fonts";
 import { CreatePortfolio } from "@/ui/portfolios/buttons";
 import { PortfolioTable } from "@/ui/portfolios/portfolio-table";
-import { portfolios } from "@/lib/placeholder-data";
+import { allHoldings, portfolios } from "@/lib/placeholder-data";
+import { getInitialPriceMap } from "@/lib/actions";
+import { getPortfolioValue } from "@/lib/portfolio";
 
-export default function Page() {
+export default async function Page() {
+  const initialPriceMap = await getInitialPriceMap(allHoldings);
+
+  portfolios.forEach((portfolio) => {
+    const holdings = allHoldings.filter((h) => h.portfolio_id === portfolio.id);
+    portfolio.value = getPortfolioValue(holdings, initialPriceMap);
+  });
+  
   return (
     <div className="w-[60vw] mx-auto text-left mt-[5vh]">
       <h1 className={`text-4xl font-bold mb-[5vh] ${quattrocento.className}`}>Portfolios page</h1>
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,17 @@
 'use server';
 
+import { Holding } from "./types";
+
 export async function deletePortfolio(id: string) {
   console.log(`You clicked on delete for portfolio ${id}`);
+}
+
+export async function getInitialPriceMap(holdings: Holding[]) {
+  const tickers = holdings.map((h) => h.ticker);
+  const prices = await Promise.all(
+    tickers.map(async (t) => await fetch(`http://localhost:8000/api/price?symbol=${t}`)
+      .then((res) => res.json()))
+  );
+
+  return new Map(prices.map((p) => [p.symbol, p.current_price]));
 }
